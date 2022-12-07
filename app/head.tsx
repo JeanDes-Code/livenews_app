b5a2f64cc586d08df468a18bb7d52986740e0684
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-12-07 16:53:27 +0100

    updating head component

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,12 @@
 export default function Head() {
-  return (
-    <>
-      <title>Jean Desauw - News</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
+    return (
+        <>
+            <title>The Daily Inquirer</title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
 }
